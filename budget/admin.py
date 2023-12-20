055617b86e614df3774183f2c907b959f7365396
spiderthatcodes <jessicaldickerson@outlook.com>
2023-12-20 08:40:14 -0700

    added to admin

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Bill, LineItem, Paychecks, Savings
 
-# Register your models here.
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = (
+        'company',
+        'amount'
+    )
+
+
+@admin.register(LineItem)
+class LineItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'amount_to_pay'
+    )
+
+
+@admin.register(Savings)
+class LineItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'balance',
+    )
